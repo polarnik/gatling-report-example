fda4e6fe0919ba648f753ed1691a70d33b41e53f
Smirnov Viacheslav <polarnik@users.noreply.github.com>
2020-06-21 19:46:51 +0300

    Update start.test.sbt.sh

--- start.test.sbt.sh
+++ start.test.sbt.sh
@@ -2,4 +2,4 @@
 
 GATLING_ROOT_PATH_PREFIX=`./start.gatling.rootPathPrefix.sh`
 
-GATLING_ROOT_PATH_PREFIX=$GATLING_ROOT_PATH_PREFIX sbt -Dgatling.data.graphite.rootPathPrefix=XXXXXXX  "gatling:testOnly io.qaload.gatling.reportExample.simulation.OpenModel_AtOnceUsers"
+GATLING_ROOT_PATH_PREFIX=$GATLING_ROOT_PATH_PREFIX sbt -Dgatling.data.graphite.rootPathPrefix=$GATLING_ROOT_PATH_PREFIX  "gatling:testOnly io.qaload.gatling.reportExample.simulation.OpenModel_AtOnceUsers"
